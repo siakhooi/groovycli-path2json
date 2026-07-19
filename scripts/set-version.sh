adaaa35a23bdcae0c43fa4c589bdee3e402f220a
Siak Hooi <73884114+siakhooi@users.noreply.github.com>
2026-07-19 19:34:04 +0800

    upgrade gradle 5 (#68)

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -6,5 +6,5 @@ set -e
 
 sed -i 'path2json/gradle.properties' -e 's@version = .*@version = '"$RELEASE_VERSION"'@g'
 sed -i 'path2json/build.gradle' -e 's@path2json-.*-all.jar@path2json-'"$RELEASE_VERSION"'-all.jar@g' \
-  -e "s@^    version .*@    version '$RELEASE_VERSION'@g"
+  -e "s@^    version = .*@    version = '$RELEASE_VERSION'@g"
 sed -i 'path2json/src/scripts/path2json' -e 's@readonly VERSION=.*@readonly VERSION='"$RELEASE_VERSION"'@g'
